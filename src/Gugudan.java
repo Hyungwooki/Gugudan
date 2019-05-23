6efd748b139c20f314a9d6ab92f7c71be2ea5e6e
Master <Master@DESKTOP-B4RFA4B.fritz.box>
2019-05-23 15:00:44 +0200

    구구단 2,3단

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,33 +1,24 @@
 
-public class Gugudan {
-    public static void main (String [] a) {
-       int x = 2;
-       int q = 1;
-       int b = 2;
-       int c = 3;
-       int d = 4;
-       int e = 5;
-       int f = 6;
-       int g = 7;
-       int h = 8;
-       int i = 9;
-       int m1 = x*q;
-       int m2 = x*b;
-       int m3 = x*c;
-       int m4 = x*d;
-       int m5 = x*e;
-       int m6 = x*f;
-       int m7 = x*g;
-       int m8 = x*h;
-       int m9 = x*i;
-       System.out.println(m1);
-       System.out.println(m2);
-       System.out.println(m3);
-       System.out.println(m4);
-       System.out.println(m5);
-       System.out.println(m6);
-       System.out.println(m7);
-       System.out.println(m8);
-       System.out.println(m9);
-       }
-  }
+   public class Gugudan {
+	   public static void main (String [] a) {
+		 // 2,3 단
+		   System.out.println(2*1);
+		   System.out.println(2*2);
+		   System.out.println(2*3);
+		   System.out.println(2*4);
+		   System.out.println(2*5);
+		   System.out.println(2*6);
+		   System.out.println(2*7);
+		   System.out.println(2*8);
+		   System.out.println(2*9);
+		   System.out.println(3*1);
+		   System.out.println(3*2);
+		   System.out.println(3*3);
+		   System.out.println(3*4);
+		   System.out.println(3*5);
+		   System.out.println(3*6);
+		   System.out.println(3*7);
+		   System.out.println(3*8);
+		   System.out.println(3*9);
+	   }
+   }
