8cc63052a7b1a08c515322a1cbe203f0089d7675
Master <Master@DESKTOP-B4RFA4B.fritz.box>
2019-05-23 14:56:07 +0200

    구구단 첫 시도

---
+++ src/Gugudan.java
@@ -0,0 +1,33 @@
+
+public class Gugudan {
+    public static void main (String [] a) {
+       int x = 2;
+       int q = 1;
+       int b = 2;
+       int c = 3;
+       int d = 4;
+       int e = 5;
+       int f = 6;
+       int g = 7;
+       int h = 8;
+       int i = 9;
+       int m1 = x*q;
+       int m2 = x*b;
+       int m3 = x*c;
+       int m4 = x*d;
+       int m5 = x*e;
+       int m6 = x*f;
+       int m7 = x*g;
+       int m8 = x*h;
+       int m9 = x*i;
+       System.out.println(m1);
+       System.out.println(m2);
+       System.out.println(m3);
+       System.out.println(m4);
+       System.out.println(m5);
+       System.out.println(m6);
+       System.out.println(m7);
+       System.out.println(m8);
+       System.out.println(m9);
+       }
+  }
