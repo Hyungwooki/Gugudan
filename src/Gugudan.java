94f4897248faa41a815d008f1e60d9cb8d436308
Master <Master@DESKTOP-B4RFA4B.fritz.box>
2019-05-23 22:31:03 +0200

    Scanner. 스캔 계산기 (구구단)

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,48 +1,19 @@
 import java.util.Scanner;
 public class Gugudan {
 	public static void main (String [] a) {
-		//4단
-		System.out.println ("4단");
-		int result = (4 * 1);
-		System.out.println(result);
-		result = (4 * 2);
-		System.out.println(result);
-		result = (4 * 3);
-		System.out.println(result);
-		result = (4 * 4);
-		System.out.println(result);
-		result = (4 * 5);
-		System.out.println(result);
-		result = (4 * 6);
-		System.out.println(result);
-		result = (4 * 7);
-		System.out.println(result);
-		result = (4 * 8);
-		System.out.println(result);
-		result = (4 * 9);
-		System.out.println(result);
 		
 		System.out.println("1부터 9까지 곱해드립니다. 수를 입력하시오 : ");
 		Scanner scanner = new Scanner(System.in);
 		int number = scanner.nextInt();
+		System.out.println(number * 1);
+		System.out.println(number * 2);
+		System.out.println(number * 3);
+		System.out.println(number * 4);
+		System.out.println(number * 5);
+		System.out.println(number * 6);
+		System.out.println(number * 7);
+		System.out.println(number * 8);
+		System.out.println(number * 9);
 		
-		int resul = (number * 1);
-		System.out.println(resul);
-		resul = (number * 2);
-		System.out.println(resul);
-		resul = (number * 3);
-		System.out.println(resul);
-		resul = (number * 4);
-		System.out.println(resul);
-		resul = (number * 5);
-		System.out.println(resul);
-		resul = (number * 6);
-		System.out.println(resul);
-		resul = (number * 7);
-		System.out.println(resul);
-		resul = (number * 8);
-		System.out.println(resul);
-		resul = (number * 9);
-		System.out.println(resul);
 	}
 }
