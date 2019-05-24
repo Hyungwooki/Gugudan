a9986086d9b8dce423b11f8edd2e3e5c2e21478c
Master <Master@DESKTOP-B4RFA4B.fritz.box>
2019-05-24 10:59:45 +0200

    6,7단 while, for 구문 연습

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,14 +1,13 @@
 public class Gugudan {
 	public static void main (String [] a) {
-		System.out.println("7단을 외자~");
-		
+		System.out.println("while, for 구문 연습");
 		int i = 1;
-		
 		while (i < 10) {
-		
-		System.out.println(i * 7);
-		
-		i++;
+			System.out.println(6 * i);
+			i = i + 1;
+		}
+		for (int k = 1; k < 10; k++) {
+			System.out.println(7 * k);
 		}
 	}
 }
