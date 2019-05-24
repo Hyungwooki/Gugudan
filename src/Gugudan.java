a529f7917da5760ba89a1cfb950bb1a55b9442b7
Master <Master@DESKTOP-B4RFA4B.fritz.box>
2019-05-24 15:41:17 +0200

    8,9단 조건문, while for 구문 연습.

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,20 +1,25 @@
 import java.util.Scanner;
 public class Gugudan {
-	public static void main (String [] a) {
-		System.out.println("조건문 및 for구문 연습");
-		System.out.println("구구단을 외자! 구구단을 외자! : ");
-			Scanner scanner = new Scanner(System.in);
-			int number = scanner.nextInt();
-			System.out.println("사용자가 입력한 값은 : " +number);
-			
-			if (number < 2) {
-				System.out.println("오류 error");
-			} else if (number > 9) {
-				System.out.println("오류 error");
-			} else {
-				for (int i = 1; i <10; i ++) { // i = i + 1
-					System.out.println(number * i);
-				}
-			}
+public static void main (String [] a) {
+	System.out.println("8,9단 조건문, while, for 구문 연습");
+	System.out.println("구구단을 외자! 구구단을 외자!");
+	Scanner scanner = new Scanner(System.in);
+	int number = scanner.nextInt();
+	System.out.println("당신이 입력한 수는 :" +number);
+	
+	if (number < 2) {
+		System.out.println("오류 error");
+	} else if (number > 9) {
+		System.out.println("오류 error");
+	} else {
+		int i = 1;
+		while (i < 10) {
+			System.out.println(number * i);
+			i++;
+		}
+		for (int k = 1; k < 10; k ++) {
+			System.out.println(number * k);
+		}
 	}
+}
 }
