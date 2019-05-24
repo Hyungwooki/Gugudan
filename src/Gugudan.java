9d3bd3fe48a8f0cb7395118d22a076cf034d0d1a
Master <Master@DESKTOP-B4RFA4B.fritz.box>
2019-05-24 11:37:19 +0200

    8,9단 조건문 for문과 결합하여 연습.

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,13 +1,21 @@
+import java.util.Scanner;
 public class Gugudan {
 	public static void main (String [] a) {
-		System.out.println("while, for 구문 연습");
-		int i = 1;
-		while (i < 10) {
-			System.out.println(6 * i);
-			i = i + 1;
+		System.out.println("8,9단 값 입력 및 조건문 연습");
+		System.out.println("구구단을 외자 구구단을 외자 ! : ");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		
+		for (int i = 1; i < 10; i ++) {
+		System.out.println(number * i);
 		}
-		for (int k = 1; k < 10; k++) {
-			System.out.println(7 * k);
+		
+		if (number < 2) {
+			System.out.println("수가 2보다 작습니다");
+		} else if(number > 9) {
+			System.out.println("수가 9보다 큽니다");
+		} else {
+			System.out.println("앙 기모띠");
 		}
 	}
 }
