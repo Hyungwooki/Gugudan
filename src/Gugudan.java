310ba2d12be6a4e11482bf078c9535e2b5d1b9d6
Master <Master@DESKTOP-B4RFA4B.fritz.box>
2019-05-24 10:25:33 +0200

    while 반복문 연습

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,19 +1,14 @@
-import java.util.Scanner;
 public class Gugudan {
 	public static void main (String [] a) {
+		System.out.println("7단을 외자~");
 		
-		System.out.println("1부터 9까지 곱해드립니다. 수를 입력하시오 : ");
-		Scanner scanner = new Scanner(System.in);
-		int number = scanner.nextInt();
-		System.out.println(number * 1);
-		System.out.println(number * 2);
-		System.out.println(number * 3);
-		System.out.println(number * 4);
-		System.out.println(number * 5);
-		System.out.println(number * 6);
-		System.out.println(number * 7);
-		System.out.println(number * 8);
-		System.out.println(number * 9);
+		int i = 1;
 		
+		while (i < 10) {
+		
+		System.out.println(i * 7);
+		
+		i++;
+		}
 	}
 }
