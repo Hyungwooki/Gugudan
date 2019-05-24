002c363e1373f028def4c0569dc3c5e867639ed0
Master <Master@DESKTOP-B4RFA4B.fritz.box>
2019-05-24 11:53:17 +0200

    8,9단 조건문 완성본.

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -5,17 +5,16 @@ public class Gugudan {
 		System.out.println("구구단을 외자 구구단을 외자 ! : ");
 		Scanner scanner = new Scanner(System.in);
 		int number = scanner.nextInt();
-		
-		for (int i = 1; i < 10; i ++) {
-		System.out.println(number * i);
-		}
+		System.out.println("사용자가 입력한 값 : " +number);
 		
 		if (number < 2) {
-			System.out.println("수가 2보다 작습니다");
+			System.out.println("값을 잘못 입력했습니다");
 		} else if(number > 9) {
-			System.out.println("수가 9보다 큽니다");
+			System.out.println("값을 잘못 입력했습니다");
 		} else {
-			System.out.println("앙 기모띠");
+			for (int i = 1; i < 10; i ++) {
+				System.out.println(number * i);
+				}
 		}
 	}
 }
