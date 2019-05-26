68774c87abc874cc94caeac3ee74198771a3a874
Master <Master@DESKTOP-B4RFA4B.fritz.box>
2019-05-26 12:47:09 +0200

    구구단 2중 for문 연습

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,25 +1,11 @@
 
 public class Gugudan {
 	public static void main (String [] args) {
-		int[] result = new int[9];
-		
-		for (int k = 2; k < 10; k++) {
-			
-		
-		for (int i = 0; i < result.length; i++) {
-			result[i] = k * (i + 1);
+	for (int i = 2; i < 10; i++) {
+		System.out.println(i+"단");
+		for (int k = 1; k < 10; k++) {
+			System.out.println(i+"*"+k+"="+i*k);
 		}
-		
-		for (int i = 0; i < result.length; i++) {
-			System.out.println(result[i]);
-		}
-		}
-		
-		
- 	
-
-for (int k = 2; k < 10; k++) {
-	System.out.println(k);
-}
-}
+	}
+	}
 }
