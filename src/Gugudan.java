bb9b6ab36c66fac50b8585d27f404380d9faab7d
Master <Master@DESKTOP-B4RFA4B.fritz.box>
2019-05-26 13:29:11 +0200

    메소드 활용 구구단 출력..

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,12 +1,23 @@
-
 public class Gugudan {
-	public static void main (String [] args) {
-		System.out.println("구구단을 외자! 구구단을 외자!");
-		for (int i = 2; i <= 9; i++) {
-			System.out.println(i+"단");
-			for (int k = 1; k <= 9; k++) {
-				System.out.println(i + "*" + k + "=" + i*k);
-			}
+	public static int[] calculate(int times) {
+		int[] result = new int[9];
+		for (int i = 0; i < result.length; i++) {
+			result[i] = times * (i + 1);
+		}
+		return result;
+	}
+	
+	public static void print(int[] result) {
+		for (int i = 0; i < result.length; i++) {
+			System.out.println(result[i]);
+		}
+	}
+	
+	public static void main (String [] a) {
+		for (int i = 2; i <=9; i++) {
+			int[] result = calculate(i);
+			print(result);
+			
 		}
 	}
 }
