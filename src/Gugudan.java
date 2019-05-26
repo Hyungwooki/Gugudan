e7a4e31b31b3e68ef9117c0e695139d1174670e3
Master <Master@DESKTOP-B4RFA4B.fritz.box>
2019-05-26 12:51:24 +0200

    구구단 2중 for문 연습

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,11 +1,12 @@
 
 public class Gugudan {
 	public static void main (String [] args) {
-	for (int i = 2; i < 10; i++) {
-		System.out.println(i+"단");
-		for (int k = 1; k < 10; k++) {
-			System.out.println(i+"*"+k+"="+i*k);
+		System.out.println("구구단을 외자! 구구단을 외자!");
+		for (int i = 2; i <= 9; i++) {
+			System.out.println(i+"단");
+			for (int k = 1; k <= 9; k++) {
+				System.out.println(i + "*" + k + "=" + i*k);
+			}
 		}
-	}
 	}
 }
