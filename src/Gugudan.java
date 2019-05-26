0325ce27e4c562a4117d329e6174f411edb938fe
Master <Master@DESKTOP-B4RFA4B.fritz.box>
2019-05-26 12:41:36 +0200

    2중 for문 첫 시도

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,19 +1,25 @@
+
 public class Gugudan {
-	public static void main (String [] a) {
-		int [] result = new int[9];
-		for (int i = 0; i <= result.length;  i++) {
-			result[0] = 1;
-			result[1] = 2;
-			result[2] = 3;
-			result[3] = 4;
-			result[4] = 5;
-			result[5] = 6;
-			result[6] = 8;
-			result[7] = 7;
-			result[8] = 9;
+	public static void main (String [] args) {
+		int[] result = new int[9];
+		
+		for (int k = 2; k < 10; k++) {
+			
+		
+		for (int i = 0; i < result.length; i++) {
+			result[i] = k * (i + 1);
 		}
-		for (int i = 0; i <= result.length; i++) {
-			System.out.println(result[i] * 2);
+		
+		for (int i = 0; i < result.length; i++) {
+			System.out.println(result[i]);
 		}
-	}
+		}
+		
+		
+ 	
+
+for (int k = 2; k < 10; k++) {
+	System.out.println(k);
+}
+}
 }
